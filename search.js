efff936b7d2331cbbbc6a06011119764c169f860
Noel Guillen <noelblas274@gmail.com>
2023-04-20 15:17:35 -0400

    Completed javascript search functionality

--- search.js
+++ search.js
@@ -1,30 +1,37 @@
-const bookSearch = document.querySelector(".content-nav input").value;
+const bookSearch = document.querySelector(".content-nav input");
 const bookTitle = document.querySelectorAll(".book-title"); // returns an array
-const bookDetails = document.querySelectorAll(".card-details p"); // returns an array of 3 values
+const bookAuthor = document.querySelectorAll(".author");
+const bookDate = document.querySelectorAll(".date");
+const bookPublish = document.querySelectorAll(".publisher");
 const bookImg = document.querySelectorAll(".card-img img"); // returns an array;
+const bookButton = document.querySelector(".content-nav img");
 
 
 const apiKey = "AIzaSyBkVNpp07djnpcl_ueGOP6467hRX04BPAk";
-const query = bookSearch;
-const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`;
-
-
-
-fetch(url)
-.then(result => result.json())
-.then(bookData => {
-    console.log(bookData.items[0].volumeInfo);
-    setBookCard(bookData)
-})
-.catch(error => console.log(error));
-
 
 const setBookCard = (bookData) => {
     for(let i = 0; i < 6; i++){
         bookTitle[i].innerText = bookData.items[i].volumeInfo.title;
         bookImg[i].setAttribute("src", bookData.items[i].volumeInfo.imageLinks.thumbnail);
-        // bookDetails[0].innerHTML = `Author: <span>${bookData.items[i].volumeInfo.authors[0]}</span>`
+        bookAuthor[i].innerHTML = `Author: <span>${bookData.items[i].volumeInfo.authors}</span>`;
+        bookDate[i].innerHTML = `Published Date: <span>${bookData.items[i].volumeInfo.publishedDate}`;
+        bookPublish[i].innerHTML = `Publisher: <span>${bookData.items[i].volumeInfo.publisher}</span>`
     }
 };
 
-setBookCard()
+const makeBookRequest = () => {
+    const query = bookSearch.value;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`;
+    fetch(url)
+    .then(result => result.json())
+    .then(bookData => {
+    setBookCard(bookData)
+})
+.catch(error => console.log(error));
+};
+
+
+bookButton.addEventListener("click", ()=>{
+    makeBookRequest();
+    setBookCard();
+})
