1ddc4c83981666c72225cc7271795daf1a913d3c
Noel Guillen <noelblas274@gmail.com>
2023-04-20 21:15:22 -0400

    applied comments

--- search.js
+++ search.js
@@ -14,13 +14,15 @@ const apiKey = "AIzaSyBkVNpp07djnpcl_ueGOP6467hRX04BPAk";
 //a function to set all of the empty elements and fill in the book data.
 const setBookCard = (bookData) => {
     // a loop to append the data for each one. Doing 6 due to there only being 6 cards within html
+    //if no book is found return an alert
     if(!bookData.items){
         alert("Sorry No Books Found");
     }else{
         for(let i = 0; i < 6; i++){
+            //if information for the following not available return a placement holder
             if(!bookData.items[i].volumeInfo.title){
                 bookTitle[i].innerText = "Information Unavailable"
-            } else if(!bookData.items[i].volumeInfo.imageLinks.thumbnail){
+            }else if(!bookData.items[i].volumeInfo.imageLinks.thumbnail){
                 bookImg[i].setAttribute("src", "https://media.istockphoto.com/id/1342278794/vector/error-icon-in-the-form-of-pixel-graphics-error-means-that-this-site-is-unavailable-cant-be.jpg?s=612x612&w=0&k=20&c=OAAYswfa8cZ2WfiCKDKRO2MHNkfGAJ4QWVYt2SoRA6k=");
             }else if(!bookData.items[i].volumeInfo.authors){
                 bookAuthor[i].innerHTML = `Author: <span>Information Unavailable</span>`;
@@ -29,7 +31,8 @@ const setBookCard = (bookData) => {
             }else if(!bookData.items[i].volumeInfo.publisher){
                 bookPublish[i].innerHTML = `Publisher: <span>Information Unavailable</span>`
             }else{
-                 bookTitle[i].innerText = bookData.items[i].volumeInfo.title;
+                // if information is found then display it
+                bookTitle[i].innerText = bookData.items[i].volumeInfo.title;
                 bookImg[i].setAttribute("src", bookData.items[i].volumeInfo.imageLinks.thumbnail);
                 bookAuthor[i].innerHTML = `Author: <span>${bookData.items[i].volumeInfo.authors}</span>`;
                 bookDate[i].innerHTML = `Published Date: <span>${bookData.items[i].volumeInfo.publishedDate}</span>`;
