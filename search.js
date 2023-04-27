6d02a571b4c9d1c8ce2e8a5b0b0a9c2607edd1e2
Matthew Lang Sottile <me@matthewLS.com>
2023-04-27 16:46:57 -0400

    imeplement search fix

--- search.js
+++ search.js
@@ -1,5 +1,10 @@
 import {menuIcon, openMenu} from "./navigation_module.js";
-import {bookSearch, makeBookRequest} from "./searching_module.js";
+import {bookSearch, makeBookRequest, bookButton} from "./searching_module.js";
+
+// if the user click the search button it will generate a new book search
+const bookButtonEvent = bookButton.addEventListener("click", ()=>{
+    makeBookRequest();
+})
 
 
 
